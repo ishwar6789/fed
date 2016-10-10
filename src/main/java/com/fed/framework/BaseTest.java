bcf70ed7e2578573f52559acafed1cb0d13d626c
jyothishwar casimkota <jyothishwar casimkota>
2016-10-10 23:07:55 +0530

    “test”

--- src/main/java/com/fed/framework/BaseTest.java
+++ src/main/java/com/fed/framework/BaseTest.java
@@ -4,6 +4,9 @@ public class BaseTest {
 	
 	public void setUpBase(){
 		System.out.println("Starting TestSuite");
+		GlobalVariables.setGlobalVariables();
+		System.out.println("Browser is  "+ GlobalVariables.browser);
+		System.out.println("");
 		
 	}
 	
